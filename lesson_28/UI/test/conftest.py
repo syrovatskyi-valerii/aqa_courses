7804a53732481911e6caa4f60dc35a103291e75a
Valerii Syrovatskyi <valerii.syrovatskyi@raccoongang.com>
2025-08-17 14:02:01 +0300

    upd requirements

--- lesson_28/UI/test/conftest.py
+++ lesson_28/UI/test/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import allure
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from lesson_28.UI.pages.garage_page import GaragePage
 from lesson_28.UI.pages.home_page import HomePage
@@ -21,7 +23,8 @@ def driver():
         options = Options()
         options.add_argument("--headless")
         options.add_argument("--window-size=1920,1080")
-        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(options=options,
+                                  service=Service(ChromeDriverManager().install()))
 
         yield driver
 
